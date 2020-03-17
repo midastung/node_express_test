d001f48cce9eb7be9fb5726ebabe926c4e20e96a
firekilin <wl00161839@gmail.com>
2020-03-18 04:16:44 +0800

    修改日期為yymmdd 增加篩選功能 修正資料庫連結

--- public/javascripts/indexScript.js
+++ public/javascripts/indexScript.js
@@ -15,8 +15,9 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//window.onclick = function(event) {
+//  if (event.target == modal) {
+ //   modal.style.display = "none";
+//  }
+//}
+
